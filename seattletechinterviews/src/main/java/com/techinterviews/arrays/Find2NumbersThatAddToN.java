53a240f506af7fba7e5f5d1b77ccc0e532d6e082
rsalota <rsalota@zulily.com>
2015-03-05 21:52:37 -0800

    Adding some questions

--- seattletechinterviews/src/main/java/com/techinterviews/arrays/Find2NumbersThatAddToN.java
+++ seattletechinterviews/src/main/java/com/techinterviews/arrays/Find2NumbersThatAddToN.java
@@ -1,7 +1,8 @@
 package com.techinterviews.arrays;
 
 /**
- * Find 2 numbers in an array that add up to a given number.
+ * Find 2 numbers in an array that add up to a given number. 
+ * Please provide a solution with o(n), o(nlogn), o(n^2)  
  * 
  * @author rsalota
  *
