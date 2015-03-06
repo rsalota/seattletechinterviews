f6d9d3ce466c0a8e8ec86d836feadef00a9e5498
rsalota <rsalota@zulily.com>
2015-03-05 21:56:50 -0800

    Cleaning up a little

--- seattletechinterviews/src/main/java/com/techinterviews/lists/LinkedList.java
+++ seattletechinterviews/src/main/java/com/techinterviews/lists/LinkedList.java
@@ -2,19 +2,10 @@ package com.techinterviews.lists;
 
 public class LinkedList {
 	ListNode head;
-	/**
-	 * Please implement and execute the <code>AddLinkedListTest</code> 
-	 * 
-	 * @param k
-	 */
+	
 	public void add(int k) {
 	}
 
-	/**
-	 * Please implement and execute the <code>InsertAtLinkedListTest</code>
-	 * @param k
-	 * @param at
-	 */
 	public void insert(int k, int at) {
 		
 	}
@@ -24,14 +15,6 @@ public class LinkedList {
 
 	public ListNode get(int at) {
 		ListNode current = head;
-		while (current != null) {
-			if (at == 0) {
-				return current;
-			} else {
-				current = current.next;
-				at--;
-			}
-		}
 		return current;
 	}
 }
